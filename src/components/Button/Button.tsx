513264f11f4a5151286c24c2848c5bd1d4a71183
svtkrp <svtkrp@gmail.com>
2022-07-05 17:47:06 +0300

    add scss

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import './Button.scss';
 
 export interface Props {
   title: string;
+  primary?: boolean;
+  secondary?: boolean;
+  accent?: boolean;
+  success?: boolean;
+  warning?: boolean;
+  danger?: boolean;
+  outlined?: boolean;
 }
 
 const Button = (props: Props) => {
-  return <button>{props.title}</button>;
+  return (
+    <button className={
+      (!!props.primary ? ' primary' : '')
+    + (!!props.secondary ? ' secondary' : '')
+    + (!!props.accent ? ' accent' : '')
+    + (!!props.success ? ' success' : '')
+    + (!!props.warning ? ' warning' : '')
+    + (!!props.danger ? ' danger' : '')
+    + (!!props.outlined ? ' outlined' : '')
+    }>
+      {props.title}
+    </button>
+  );
 };
 
 export default Button;
